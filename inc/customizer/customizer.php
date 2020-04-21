6de024929823bc67d4207860f92d1cffe0929df7
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-04-21 09:43:31 +0200

    added own section file for website settings in Customizer

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -21,6 +21,7 @@ require( get_template_directory() . '/inc/customizer/sections/customizer-blog.ph
 require( get_template_directory() . '/inc/customizer/sections/customizer-post.php' );
 require( get_template_directory() . '/inc/customizer/sections/customizer-magazine.php' );
 require( get_template_directory() . '/inc/customizer/sections/customizer-info.php' );
+require( get_template_directory() . '/inc/customizer/sections/customizer-website.php' );
 
 /**
  * Registers Theme Options panel and sets up some WordPress core settings
@@ -40,70 +41,8 @@ function mercia_customize_register_options( $wp_customize ) {
 	// Change default background section.
 	$wp_customize->get_control( 'background_color' )->section = 'background_image';
 	$wp_customize->get_section( 'background_image' )->title   = esc_html__( 'Background', 'mercia' );
-
-	// Add postMessage support for site title and description.
-	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
-
-	// Add selective refresh for site title and description.
-	$wp_customize->selective_refresh->add_partial( 'blogname', array(
-		'selector'        => '.site-title a',
-		'render_callback' => 'mercia_customize_partial_blogname',
-	) );
-	$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-		'selector'        => '.site-description',
-		'render_callback' => 'mercia_customize_partial_blogdescription',
-	) );
-
-	// Add Display Site Title Setting.
-	$wp_customize->add_setting( 'mercia_theme_options[site_title]', array(
-		'default'           => true,
-		'type'              => 'option',
-		'transport'         => 'postMessage',
-		'sanitize_callback' => 'mercia_sanitize_checkbox',
-	) );
-
-	$wp_customize->add_control( 'mercia_theme_options[site_title]', array(
-		'label'    => esc_html__( 'Display Site Title', 'mercia' ),
-		'section'  => 'title_tagline',
-		'settings' => 'mercia_theme_options[site_title]',
-		'type'     => 'checkbox',
-		'priority' => 10,
-	) );
-
-	// Add Display Tagline Setting.
-	$wp_customize->add_setting( 'mercia_theme_options[site_description]', array(
-		'default'           => true,
-		'type'              => 'option',
-		'transport'         => 'postMessage',
-		'sanitize_callback' => 'mercia_sanitize_checkbox',
-	) );
-
-	$wp_customize->add_control( 'mercia_theme_options[site_description]', array(
-		'label'    => esc_html__( 'Display Tagline', 'mercia' ),
-		'section'  => 'title_tagline',
-		'settings' => 'mercia_theme_options[site_description]',
-		'type'     => 'checkbox',
-		'priority' => 11,
-	) );
 }
 add_action( 'customize_register', 'mercia_customize_register_options' );
-
-
-/**
- * Render the site title for the selective refresh partial.
- */
-function mercia_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-
-/**
- * Render the site tagline for the selective refresh partial.
- */
-function mercia_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
 
 
 /**
---
+++ inc/customizer/sections/customizer-website.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Site Identity Settings
+ *
+ * Register settings to hide site title and tagline in Site Identity section
+ *
+ * @package Mercia
+ */
+
+/**
+ * Adds Site Title settings to the Customizer
+ *
+ * @param object $wp_customize / Customizer Object.
+ */
+function mercia_customize_register_website_settings( $wp_customize ) {
+
+	// Add postMessage support for site title and description.
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+
+	// Add selective refresh for site title and description.
+	$wp_customize->selective_refresh->add_partial( 'blogname', array(
+		'selector'        => '.site-title a',
+		'render_callback' => 'mercia_customize_partial_blogname',
+	) );
+	$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+		'selector'        => '.site-description',
+		'render_callback' => 'mercia_customize_partial_blogdescription',
+	) );
+
+	// Add Display Site Title Setting.
+	$wp_customize->add_setting( 'mercia_theme_options[site_title]', array(
+		'default'           => true,
+		'type'              => 'option',
+		'transport'         => 'postMessage',
+		'sanitize_callback' => 'mercia_sanitize_checkbox',
+	) );
+
+	$wp_customize->add_control( 'mercia_theme_options[site_title]', array(
+		'label'    => esc_html__( 'Display Site Title', 'mercia' ),
+		'section'  => 'title_tagline',
+		'settings' => 'mercia_theme_options[site_title]',
+		'type'     => 'checkbox',
+		'priority' => 10,
+	) );
+
+	// Add Display Tagline Setting.
+	$wp_customize->add_setting( 'mercia_theme_options[site_description]', array(
+		'default'           => true,
+		'type'              => 'option',
+		'transport'         => 'postMessage',
+		'sanitize_callback' => 'mercia_sanitize_checkbox',
+	) );
+
+	$wp_customize->add_control( 'mercia_theme_options[site_description]', array(
+		'label'    => esc_html__( 'Display Tagline', 'mercia' ),
+		'section'  => 'title_tagline',
+		'settings' => 'mercia_theme_options[site_description]',
+		'type'     => 'checkbox',
+		'priority' => 11,
+	) );
+}
+add_action( 'customize_register', 'mercia_customize_register_website_settings' );
+
+
+/**
+ * Render the site title for the selective refresh partial.
+ */
+function mercia_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ */
+function mercia_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
