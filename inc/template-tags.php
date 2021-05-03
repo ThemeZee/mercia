f35fb5c1d43fa58dc1e0778193c3de718719dc04
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-05-03 17:54:52 +0200

    fixed two minor AMP bugs

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -366,7 +366,7 @@ if ( ! function_exists( 'mercia_pagination' ) ) :
 
 		the_posts_pagination( array(
 			'mid_size'  => 2,
-			'prev_text' => '&laquo<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'mercia' ) . '</span>',
+			'prev_text' => '&laquo;<span class="screen-reader-text">' . esc_html_x( 'Previous Posts', 'pagination', 'mercia' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . esc_html_x( 'Next Posts', 'pagination', 'mercia' ) . '</span>&raquo;',
 		) );
 
