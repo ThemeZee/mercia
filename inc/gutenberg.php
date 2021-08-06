b7327a0fdaed9b0afa940268037753d91756b231
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-08-06 16:15:58 +0200

    added editor block styles

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -91,7 +91,7 @@ add_action( 'after_setup_theme', 'mercia_gutenberg_support' );
 function mercia_block_editor_assets() {
 
 	// Enqueue Editor Styling.
-	wp_enqueue_style( 'mercia-editor-styles', get_theme_file_uri( '/assets/css/gutenberg-styles.css' ), array(), '20210306', 'all' );
+	wp_enqueue_style( 'mercia-editor-styles', get_theme_file_uri( '/assets/css/editor-styles.css' ), array(), '20210306', 'all' );
 
 	// Enqueue Page Template Switcher Editor plugin.
 	#wp_enqueue_script( 'mercia-page-template-switcher', get_theme_file_uri( '/assets/js/page-template-switcher.js' ), array( 'wp-blocks', 'wp-element', 'wp-edit-post' ), '20210306' );
